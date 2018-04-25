68a27254b8268277076a1e110436f98ef1e57322
mithrand <munoztorres.antonio@gmail.com>
2018-04-25 22:21:23 +0200

    added scss and tsconfig
    added tsconfig and scss support

--- webpack.common.js
+++ webpack.common.js
@@ -33,7 +33,17 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+    }
     ]
   },
   optimization:{
